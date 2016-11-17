8260b2c6d57accd4279ae0abf42bc7d1d831cf68
Thomas Rivas <rivassi.thomas@gmail.com>
2016-11-16 16:44:29 -0800

    write first test

---
+++ app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
@@ -0,0 +1,33 @@
+package com.epicodus.myrestaurants;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by Guest on 11/16/16.
+ */
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        //configure our test class so it knows which Activity we will use to write our tests
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+}
